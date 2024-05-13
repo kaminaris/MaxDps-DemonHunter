0028b2d82110141447c5d10c4c7f0290b1878cd6
doadin <tbkizle@gmail.com>
2024-05-12 21:42:34 -0400

    Formatting Changes and Update Print

--- Main.lua
+++ Main.lua
@@ -1,28 +1,28 @@
-local addonName, addonTable = ...;
-_G[addonName] = addonTable;
+local addonName, addonTable = ...
+_G[addonName] = addonTable
 
 --- @type MaxDps
 if not MaxDps then return end
 
-local MaxDps = MaxDps;
+local MaxDps = MaxDps
 
-local DemonHunter = MaxDps:NewModule('DemonHunter');
-addonTable.DemonHunter = DemonHunter;
+local DemonHunter = MaxDps:NewModule('DemonHunter')
+addonTable.DemonHunter = DemonHunter
 
 DemonHunter.spellMeta = {
 	__index = function(t, k)
-		print('Spell Key ' .. k .. ' not found!');
+		print('Spell Key ' .. k .. ' not found!')
 	end
 }
 
 function DemonHunter:Enable()
 	if MaxDps.Spec == 1 then
-		MaxDps.NextSpell = DemonHunter.Havoc;
-		MaxDps:Print(MaxDps.Colors.Info .. 'DemonHunter - Havoc');
+		MaxDps.NextSpell = DemonHunter.Havoc
+		MaxDps:Print(MaxDps.Colors.Info .. 'DemonHunter - Havoc', "info")
 	elseif MaxDps.Spec == 2 then
-		MaxDps.NextSpell = DemonHunter.Vengeance;
-		MaxDps:Print(MaxDps.Colors.Info .. 'DemonHunter - Vengeance');
+		MaxDps.NextSpell = DemonHunter.Vengeance
+		MaxDps:Print(MaxDps.Colors.Info .. 'DemonHunter - Vengeance', "info")
 	end
 
-	return true;
+	return true
 end
