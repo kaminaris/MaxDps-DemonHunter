7e2710d2f0d24b33f01580ab954d9d9c8778d3e7
tbkizle <tbkizle@gmail.com>
2026-03-09 00:05:34 -0400

    Update Devourer

--- Specialization/Devourer.lua
+++ Specialization/Devourer.lua
@@ -31,11 +31,27 @@ function DemonHunter:Devourer()
     end
     if class and specName and MaxDps.classCooldowns[class] and MaxDps.classCooldowns[class][specName] then
         for _, spellID in pairs(MaxDps.classCooldowns[class][specName].offensive) do
-            --print("Defensive:", spellName, spellID)
+            --print("Offensive:", C_Spell.GetSpellInfo(spellID).name, spellID)
+            if MaxDps.FrameData.ACSpells and MaxDps.FrameData.ACSpells[1217605] then -- Metamorphosis
+                MaxDps.FrameData.ACSpells[1217605] = nil
+            end
+            --if MaxDps.FrameData.ACSpells and MaxDps.FrameData.ACSpells[1221150] then -- Collapsing Star
+            --    MaxDps.FrameData.ACSpells[1221150] = nil
+            --end
             if not MaxDps.FrameData.ACSpells or not MaxDps.FrameData.ACSpells[spellID] then
-                if MaxDps:CheckSpellUsable(spellID) then
-                    MaxDps:GlowCooldownMidnight(spellID, true)
-                end
+                --if MaxDps:CheckSpellUsable(spellID) then
+                --    --print("Offensive:", C_Spell.GetSpellInfo(spellID).name, spellID)
+                --    MaxDps:GlowCooldownMidnight(spellID, true)
+                --end
+                --if MaxDps:CheckSpellUsable(spellID) and MaxDps.Spells[1221150] then
+                --    --print("Offensive:", C_Spell.GetSpellInfo(spellID).name, spellID)
+                --    MaxDps:GlowCooldownMidnight(spellID, false)
+                --elseif MaxDps:CheckSpellUsable(spellID) and not MaxDps.Spells[1221150] then
+                --    --print("Offensive:", C_Spell.GetSpellInfo(spellID).name, spellID)
+                --    MaxDps:GlowCooldownMidnight(spellID, true)
+                --end
+                MaxDps:GlowCooldownMidnight(spellID, MaxDps:CheckSpellUsable(spellID))
+
             --else
             --    if MaxDps and MaxDps.Spells and MaxDps.Spells[spellID] and not InCombatLockdown() then
             --        for i=1,#MaxDps.Spells[spellID] do
